832a248aaaf08c8e1d4f4eeb5c388b4b570cddf1
vanmak <vanmakdavid.lwg@gmail.com>
2021-08-23 21:15:21 +0200

    [ADD] add stars and icons on GameDetail.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
+import {
+  playstation,
+  steam,
+  xbox,
+  nintendo,
+  apple,
+  gamepad,
+  starEmpty,
+  starFull,
+} from "img";
+
 /**
  * Media resize
  * @method smallImage
@@ -14,4 +25,36 @@ export const smallImage = (imagePath, size) => {
       )
     : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`);
   return image;
+};
+
+//GET PLATFORM IMAGES
+export const getPlatform = (platform) => {
+  switch (platform) {
+    case "PlayStation 4":
+      return playstation;
+    case "Xbox One":
+      return xbox;
+    case "PC":
+      return steam;
+    case "Nintendo Switch":
+      return nintendo;
+    case "iOS":
+      return apple;
+    default:
+      return gamepad;
+  }
+};
+
+//Get Stars
+export const getStars = (ratingGame) => {
+  const stars = [];
+  const rating = Math.floor(ratingGame);
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars.push(<img alt="star" key={i} src={starFull}></img>);
+    } else {
+      stars.push(<img alt="star" key={i} src={starEmpty}></img>);
+    }
+  }
+  return stars;
 };
